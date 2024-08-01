d50a7862cb3f2198f86793289f3841e3cb49f583
sarveshk0 <kumarsarvesh105@gmail.com>
2024-08-01 12:48:28 +0530

    changes

--- src/assets/Component/Hero.jsx
+++ src/assets/Component/Hero.jsx
@@ -11,11 +11,8 @@ const handlepdf=()=>{
 }
 
   return (
-    <div className=" container hero"> 
-         
-         
-         
-        <div className="left ">
+    <div className=" hero"> 
+         <div className="left ">
        <div className="desc">
        I am a passionate 4th year B.Tech student and a committed software developer with a strong foundation in web development and software engineering. My technical expertise includes HTML, CSS, Java, Tailwind CSS, React.js, Node.js, Firebase, and MongoDB. Over the course of my academic career, I have developed several impactful projects, such as a resume builder web app, a Razorpay clone using Tailwind CSS, and a project integrating HTML, CSS, and Firebase.
 
@@ -23,8 +20,7 @@ I am currently enhancing my skills with Next.js and three.js, and I am actively 
 
 Beyond my technical pursuits, I am an active member of my college's coding club, where I collaborate with peers to solve complex problems and share knowledge. My dedication to my studies and extracurricular activities has been recognized with the prestigious Karmyogi Scholarship.
 
-I am excited about the opportunities to apply my skills in dynamic and challenging environments, contributing to impactful projects and furthering my growth as a software developer.
-            {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sed voluptas distinctio atque ipsa temporibus quae aliquid dolores, aperiam soluta quod ad iste molestiae laborum nisi iure! Veniam, rerum. Totam ipsa impedit, laudantium nihil unde necessitatibus cum magni laboriosam similique a quae inventore eos illum soluta in tenetur quod sed? */}
+I am excited about the opportunities to apply my skills in dynamic and challenging environments, contributing to impactful projects and furthering my growth as a software developer
        </div>
      
 
